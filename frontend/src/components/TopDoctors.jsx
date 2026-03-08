e76fc239ab090f8730574d621a1392889a5dae23
Aurialedge <saperkar1862004@gmail.com>
2026-03-08 09:38:09 +0530

    Almost completed the Home page

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { doctors } from '../assets/assets'
+
+const TopDoctors = () => {
+  return (
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+        <h1 className='text-3xl font-medium'> Top Doctors to Book</h1>
+        <p className='sm:w-1/3 text-center text-sm'>Go through our extensive list of top-rated doctors</p>
+        <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+             {doctors.slice(0,10).map((item,index) => (
+                <div className='border border-red-300 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <img className='bg-red-300' src={item.image} alt={item.name} />
+                    <div className='p-4'>
+                       <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                        </div>
+                        <p>{item.name}</p>
+                        <p>
+                            {item.speciality}
+                        </p>
+                   </div>
+                </div>
+             ))}
+        </div>
+        <button>View All Doctors</button>
+    </div>
+  )
+}
+
+export default TopDoctors
